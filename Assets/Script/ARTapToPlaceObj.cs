126fc26174fb2a12a1199a9fc9115c6c389dc2ce
zimbabirbal <tamangbirbal23@gmail.com>
2020-09-08 23:02:01 +0545

    add rotate and scale

--- Assets/Script/ARTapToPlaceObj.cs
+++ Assets/Script/ARTapToPlaceObj.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -26,6 +27,12 @@ public class ARTapToPlaceObj : MonoBehaviour
     public GameObject[] UICrossButtons;
 
     private int curIndex = 0;
+    public InputField Rotate;
+    public InputField Scale;
+
+    private float _defaultRotation = 25f;
+    private float _defaultScale = 0.5f;
+    
     private void Awake()
     {
         width = (float)Screen.width / 2.0f;
@@ -37,6 +44,9 @@ public class ARTapToPlaceObj : MonoBehaviour
             UICrossButtons[i].SetActive(false);
         }
         _aRRaycastManager = GetComponent<ARRaycastManager>();
+
+        Rotate.text = "25";
+        Scale.text = "0.5";
     }
 
     // Update is called once per frame
@@ -55,12 +65,14 @@ public class ARTapToPlaceObj : MonoBehaviour
                 DisplaInfo.text = "Obj spawned new" + hitPose.position.x + "," + hitPose.position.y + "," + hitPose.position.z;
                 _spawnedObj = Instantiate(spawnObjects[curIndex], hitPose.position, hitPose.rotation);
                 tempspawnObjects[curIndex] = _spawnedObj;
+                _spawnedObj.transform.Rotate(Vector3.up, 180f);
             }
             else
             {
                 DisplaInfo.text = "Obj spawned old" + hitPose.position.x + "," + hitPose.position.y + "," + hitPose.position.z;
                 _spawnedObj.transform.position = hitPose.position;
-            }
+            }            
+            CurObjToInstantiate = _spawnedObj;
         }
     }
 
@@ -89,6 +101,76 @@ public class ARTapToPlaceObj : MonoBehaviour
         }
         touchPosition = default;
         return false;
+    }
+
+    public void RotateLeftObject()
+    {
+        if (CurObjToInstantiate != null)
+        {
+            //var y = CurObjToInstantiate.transform.rotation.y;
+            //y += 20f;
+            //CurObjToInstantiate.transform.localRotation = Quaternion.Euler(0,y,0);
+            if (Single.TryParse(Rotate.text, out float output))
+            {
+                CurObjToInstantiate.transform.Rotate(Vector3.up, output);
+            }
+            else
+            {
+                CurObjToInstantiate.transform.Rotate(Vector3.up, _defaultRotation);
+            }
+        }
+    }
+    public void RotateRightObject()
+    {
+        if (CurObjToInstantiate != null)
+        {
+            if (Single.TryParse(Rotate.text, out float output))
+            {
+                CurObjToInstantiate.transform.Rotate(Vector3.up, -output);
+            }
+            else
+            {
+                CurObjToInstantiate.transform.Rotate(Vector3.up, -_defaultRotation);
+            }
+        }
+    }
+
+    public void ScaleUpObject()
+    {
+        if (CurObjToInstantiate != null)
+        {
+            if (Single.TryParse(Scale.text, out float output))
+            {
+                var curScale = CurObjToInstantiate.transform.localScale;
+                var temp = new Vector3(curScale.x + output, curScale.y + output, curScale.z + output);
+                CurObjToInstantiate.transform.localScale = temp;
+            }
+            else
+            {
+                var curScale = CurObjToInstantiate.transform.localScale;
+                var temp = new Vector3(curScale.x + _defaultScale, curScale.y + _defaultScale, curScale.z + _defaultScale);
+                CurObjToInstantiate.transform.localScale = temp;
+            }
+
+        }
+    }
+    public void ScaleDownObject()
+    {
+        if (CurObjToInstantiate != null)
+        {
+            if (Single.TryParse(Scale.text, out float output))
+            {
+                var curScale = CurObjToInstantiate.transform.localScale;
+                var temp = new Vector3(curScale.x - output, curScale.y - output, curScale.z - output);
+                CurObjToInstantiate.transform.localScale = temp;
+            }
+            else
+            {
+                var curScale = CurObjToInstantiate.transform.localScale;
+                var temp = new Vector3(curScale.x - _defaultScale, curScale.y - _defaultScale, curScale.z - _defaultScale);
+                CurObjToInstantiate.transform.localScale = temp;
+            }
+        }
     }
 
     public void MoveToNextScene()
---
+++ Assets/Script/TempTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TempTest : MonoBehaviour
+{
+    // Update is called once per frame
+    public GameObject CurObjToInstantiate;
+    public InputField Rotate;
+    public InputField Scale;
+
+    private float _defaultRotation = 25f;
+    private float _defaultScale = 0.5f;
+    private void Start()
+    {
+        Rotate.text = "25";
+        Scale.text = "0.5";
+    }
+    void Update()
+    {
+        
+    }
+    public void RotateLeftObject()
+    {
+        if (CurObjToInstantiate != null)
+        {
+            //var y = CurObjToInstantiate.transform.rotation.y;
+            //y += 20f;
+            //CurObjToInstantiate.transform.localRotation = Quaternion.Euler(0,y,0);
+            if (Single.TryParse(Rotate.text, out float output))
+            {
+                CurObjToInstantiate.transform.Rotate(Vector3.up, output);
+            }
+            else
+            {
+                CurObjToInstantiate.transform.Rotate(Vector3.up, _defaultRotation);
+            }
+        }
+    }
+    public void RotateRightObject()
+    {
+        if (CurObjToInstantiate != null)
+        {
+            if (Single.TryParse(Rotate.text, out float output))
+            {
+                CurObjToInstantiate.transform.Rotate(Vector3.up, output);
+            }
+            else
+            {
+                CurObjToInstantiate.transform.Rotate(Vector3.up, -_defaultRotation);
+            }
+        }
+    }
+
+    public void ScaleUpObject()
+    {
+        if (CurObjToInstantiate != null)
+        {
+            if (Single.TryParse(Scale.text, out float output))
+            {
+                var curScale = CurObjToInstantiate.transform.localScale;
+                var temp = new Vector3(curScale.x + output, curScale.y + output, curScale.z + output);
+                CurObjToInstantiate.transform.localScale = temp;
+            }
+            else
+            {
+                var curScale = CurObjToInstantiate.transform.localScale;
+                var temp = new Vector3(curScale.x + _defaultScale, curScale.y + _defaultScale, curScale.z + _defaultScale);
+                CurObjToInstantiate.transform.localScale = temp;
+            }
+            
+        }
+    }
+    public void ScaleDownObject()
+    {
+        if (CurObjToInstantiate != null)
+        {
+            if (Single.TryParse(Scale.text, out float output))
+            {
+                var curScale = CurObjToInstantiate.transform.localScale;
+                var temp = new Vector3(curScale.x - output, curScale.y - output, curScale.z - output);
+                CurObjToInstantiate.transform.localScale = temp;
+            }
+            else
+            {
+                var curScale = CurObjToInstantiate.transform.localScale;
+                var temp = new Vector3(curScale.x - _defaultScale, curScale.y - _defaultScale, curScale.z - _defaultScale);
+                CurObjToInstantiate.transform.localScale = temp;
+            }
+        }
+    }
+}
